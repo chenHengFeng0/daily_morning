c90b3ae25fbb8696a8419bf40e04d97e339c58ed
chenhengfeng <415038653@qq.com>
2022-08-27 15:02:29 +0800

    修改py文件

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ template_id = os.environ["TEMPLATE_ID"]
 
 # 用户ID
 user_id = os.environ["USER_ID"]
-
+print(user_id)
 # 天行API我的密钥KEY
 txApiKey = os.environ["TXAPI_ID"]
+print(txApiKey)
 
 # 把 user_id 以英文逗号分隔并转换成数组
 userList = user_id.split(",")
@@ -167,9 +168,7 @@ def get_birthday():
 
 # 随机生成颜色
 def get_random_color():
-    color = "#%06x" % random.randint(0, 0xFFFFFF);
-    print(color)
-    return color
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
