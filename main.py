407c3092ed9028fcf7317dc74f4b43e43683905f
chenhengfeng <415038653@qq.com>
2022-08-27 14:27:34 +0800

    修改py文件

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ template_id = os.environ["TEMPLATE_ID"]
 user_id = os.environ["USER_ID"]
 
 # 天行API我的密钥KEY
-api_key = os.environ["API_KEY"]
+api_key = os.environ["TXAPI_KEY"]
 
 # 把 user_id 以英文逗号分隔并转换成数组
 userList = user_id.split(",")
