a3ec81c6bfb535487b4df0c9c3bcab9d0c0af404
chenhengfeng <415038653@qq.com>
2022-08-27 14:14:26 +0800

    修改py文件

--- main.py
+++ main.py
@@ -6,97 +6,107 @@ import requests
 import os
 import random
 
-
+# 微信测试号的 app_id
 app_id = os.environ["APP_ID"]
+
+# 微信测试号的 app_secret
 app_secret = os.environ["APP_SECRET"]
+
 # 模板ID
 template_id = os.environ["TEMPLATE_ID"]
+
 # 用户ID
 user_id = os.environ["USER_ID"]
-#把 user_id 转换成数组
+
+# 天行API我的密钥KEY
+api_key = os.environ["API_KEY"]
+
+# 把 user_id 以英文逗号分隔并转换成数组
 userList = user_id.split(",")
-#今天的日期
+
+# 今天的日期
 today = datetime.now().strftime("%Y-%m-%d")
-#开始日期
+
+# 开始日期
 start_date = os.environ['START_DATE']
-#城市
+
+# 城市
 city = os.environ['CITY']
-#生日
+
+# 生日
 birthday = os.environ['BIRTHDAY']
 
 
-# app_id = "wxaa57ccc2ffea23bc"
-# app_secret = "654f53177e70d14e25b19e764b9f2ba1"
-#
-# template_id = "4L7c64FszgKmFd5hlR5vyQYa4zAtcGMy9ws61W6biiQ"
-#
-# user_id = "oV2R76PUmhE1KCiN1AZ_At9NQFhI,oV2R76KnAJd6LJFlRjOwpTd4QxzI"
-# userList = user_id.split(",")
-#
-# today = datetime.now().strftime("%Y-%m-%d")
-# start_date = '2017-09-01'
-# city = "武汉"
-# birthday = '1999-07-10'
 
-
+# 时间格式化统一
 td = datetime.strptime(today, "%Y-%m-%d")
 sd = datetime.strptime(start_date, "%Y-%m-%d")
 bd = datetime.strptime(birthday, "%Y-%m-%d")
 
-toDate = str(td.month)+"月"+str(td.day)+"日"
+# 自动获取今天的时间，转换成str；格式如：8月10日
+toDate = str(td.month) + "月" + str(td.day) + "日"
 
-week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
-
+# td.weekday()：获取今日是一周中的第几天（0-6），将其转换成星期几
+week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
 toWeek = week_list[td.weekday()]
 
+
+# 获取墨迹天气的API
 def get_weather():
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
     res = requests.get(url).json()
     weather = res['data']['list'][0]
-    return weather['weather'], weather['high'], weather['low'], weather['wind']
+    # 返回数据依次为：天气“晴”，当前气温，空气湿度，最高气温，最低气温，风力等级“东风4级”，pm2.5，空气质量
+    return str(weather['weather']), str(round(weather['temp'])), str(weather['humidity']), str(
+        round(weather['high'])), str(round(weather['low'])), str(weather['wind']), str(round(weather['pm25'])), str(weather['airQuality'])
 
 
-def get_count():
-    delta = td - sd
-    return delta.days
-
-
-def get_birthday():
-    next_year = int(td.year) + 1
-    nextBd = datetime.strptime(str(next_year) + "-" + str(bd.month) + "-" + str(bd.day), "%Y-%m-%d")  # 明年的生日
-    thisBd = datetime.strptime(str(int(td.year)) + "-" + str(bd.month) + "-" + str(bd.day), "%Y-%m-%d")  # 今年的生日
-    if thisBd == td:
-        return "远方的我一直在掂念着你，祝你生日快乐。"
-    elif td > thisBd:
-        d = nextBd - td
-        return "距离你的生日还有"+str(d.days)+"天"
-    elif thisBd > td:
-        d = thisBd - td
-        return "距离你的生日还有"+str(d.days)+"天"
-
-
+########################################################################################################################
+# 【START】------------------可选
+# 获取情诗API
+# url 的值为：
+# {
+#   "code":200,
+#   "msg":"success",
+#   "newslist":[
+#       {
+#           "content":"可怜无定河边骨，犹是春闺梦裏人。",
+#           "source":"陇西行",
+#           "author":"陈陶"
+#       }
+#   ]
+# }
 def get_qingShi():
-    url = "http://api.tianapi.com/qingshi/index?key=5b6cd616d69d7560b1680d54abbaa82c"
+    # "http://api.tianapi.com/qingshi/index" 替换为自己申请的API地址
+    # 请求示例：http://api.tianapi.com/qingshi/index?key=APIKEY
+    
+    url = "http://api.tianapi.com/qingshi/index?key=" + api_key
     res = requests.get(url).json()
     qingShi = res['newslist'][0]
     return qingShi['content']
 
 
+# 情话
 def get_saylove():
-    url = "http://api.tianapi.com/saylove/index?key=5b6cd616d69d7560b1680d54abbaa82c"
+    # "http://api.tianapi.com/saylove/index" 替换为自己申请的接口地址
+    url = "http://api.tianapi.com/saylove/index?key=" + api_key
     res = requests.get(url).json()
     saylove = res['newslist'][0]
     return saylove['content']
+# 【END】
 
-
+# 彩虹屁
 def get_words():
+    # 开源API
     url = "https://api.shadiao.pro/chp"
     res = requests.get(url).json()
     caiHongPi = res['data']
     return caiHongPi['text']
 
-
+# 【START】------------------可选
+#PS：如果只用 get_words() 开源API则不需要以下判断，将 data 里的 say()替换成 get_words()
 # 判断是否为质数
+# 有多个不同总类de语句输出API，做个判断，是质数返回 1
 def zhiShu(num):
     if num > 1:
         # 查看因子
@@ -114,78 +124,114 @@ def zhiShu(num):
         # print(num, "不是质数")
         return 0
 
-
+# 随机输出不同类型的语句
 def say():
+    # 取包括1-10的随机整数
     num = random.randint(1, 10)
-
+    # 如果是质数（2，3，5，7）
     if zhiShu(num) == 1:
+        # 输出彩虹屁API
         return get_words()
+    # 如果是偶数
     elif num % 2 == 0:
+        # 输出情诗API
         return get_qingShi()
     else:
+        # 其他情况输出情话API
         return get_saylove()
+# 【END】
 
 
+########################################################################################################################
+
+# 计算从指定日期开始已经过了多少天
+def get_count():
+    delta = td - sd
+    return delta.days
+
+
+# 判断今日是否是其生日
+def get_birthday():
+    next_year = int(td.year) + 1
+    nextBd = datetime.strptime(str(next_year) + "-" + str(bd.month) + "-" + str(bd.day), "%Y-%m-%d")  # 明年的生日
+    thisBd = datetime.strptime(str(int(td.year)) + "-" + str(bd.month) + "-" + str(bd.day), "%Y-%m-%d")  # 今年的生日
+    if thisBd == td:
+        return "远方的我一直在掂念着你，祝你生日快乐。"
+    elif td > thisBd:
+        d = nextBd - td
+        return "距离你的生日还有" + str(d.days) + "天"
+    elif thisBd > td:
+        d = thisBd - td
+        return "距离你的生日还有" + str(d.days) + "天"
+
+
+# 随机生成颜色
 def get_random_color():
-    return "#%06x" % random.randint(0, 0xFFFFFF)
+    color = "#%06x" % random.randint(0, 0xFFFFFF);
+    print(color)
+    return color
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, high, low, wind = get_weather()
+wea, temp, humidity, high, low, wind, pm25, airQuality = get_weather()
 
+# 将数据打包成JSON格式
 data = {
-    "date": {"value": toDate},
-    "toWeek": {"value": toWeek},
-    "city": {"value": city},
-    "weather": {"value": wea},
-    "high": {"value": str(high)},
-    "low": {"value": str(low)},
-    "wind": {"value": str(wind)},
-    "love_days": {"value": get_count()},
-    "birthday_left": {"value": str(get_birthday())},
-    "words": {"value": say(),
-              "color": get_random_color()}
+    "date": {
+        "value": toDate + " " + toWeek,
+        "color": get_random_color()
+    },
+    "city": {
+        "value": city
+    },
+    "weather": {
+        "value": wea,
+        "color": get_random_color()
+    },
+    "temp": {
+        "value": temp + " ℃",
+        "color": get_random_color()
+    },
+    "humidity": {
+        "value": humidity,
+        "color": get_random_color()
+    },
+    "high": {
+        "value": high + " ℃",
+        "color": get_random_color()
+    },
+    "low": {
+        "value": low + " ℃",
+        "color": get_random_color()
+    },
+    "wind": {
+        "value": wind,
+        "color": get_random_color()
+    },
+    "pm25": {
+        "value": pm25,
+        "color": get_random_color()
+    },
+    "airQuality": {
+        "value": airQuality,
+        "color": get_random_color()
+    },
+    "love_days": {
+        "value": get_count(),
+        "color": get_random_color()
+    },
+    "birthday_left": {
+        "value": str(get_birthday()),
+        "color": get_random_color()
+    },
+    "words": {
+        "value": "“ " + say() + " ”",   # 只使用彩虹屁 API 直接将 say() 替换为 get_words()
+        "color": get_random_color()  # 给文字赋予随机颜色
+    }
 }
 
-
 for i in userList:
-    res = wm.send_template(i, template_id, data)
-
-
-
-#
-# print(
-#     "日期" + toDate + " " + toWeek + "\n" +
-#     city + "今日" +
-#     "天气" + wea + "\n" +
-#     "最高气温：" + str(high) + "\n" +
-#     "最低气温：" + str(low) + "\n" +
-#     "wind" + str(wind) + "\n" +
-#     "认识的天数：", get_count(), "\n" +
-#     str(get_birthday()) + "\n" +
-#     say()
-# )
-
-
-
-# if __name__ == '__main__':
-#
-#     print(get_count())
-
-# def bo():
-#     next_year = int(td.year) + 1
-#     nextBd = datetime.strptime(str(next_year) + "-" + str(bd.month) + "-" + str(bd.day),"%Y-%m-%d")  # 明年的生日
-#     thisBd = datetime.strptime(str(int(td.year)) + "-" + str(bd.month) + "-" + str(bd.day),"%Y-%m-%d")  # 今年的生日
-#     print(nextBd)
-#     print(thisBd)
-#
-#     if thisBd == td:
-#         return "生日快乐"
-#     elif td > thisBd:
-#         d = nextBd - td
-#         return "距离生日还有：" + str(d.days) + "天"
-#     elif thisBd > td:
-#         d = thisBd - td
-#         return "距离生日还有：" + str(d.days) + "天"
+    # 将数据携带给微信模板并指定用户发送
+    wm.send_template(i, template_id, data)
